d731d266227f887ed636d78ebabc5868707494d1
unknown <rmtestard@gmail.com>
2019-06-02 18:15:10 +0200

    adding transition animation to our routin

--- src/App.js
+++ src/App.js
@@ -1,20 +1,63 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import { spring, AnimatedSwitch } from 'react-router-transition';
 
 import './App.css';
 import Header from './components/layouts/Header';
 import Blog from './components/Blog';
 import Photo from './components/Photo';
 
+// we need to map the `scale` prop we define below
+// to the transform style property
+function mapStyles(styles) {
+  return {
+    opacity: styles.opacity,
+    transform: `scale(${styles.scale})`
+  };
+}
+
+// wrap the `spring` helper to use a bouncy config
+function bounce(val) {
+  return spring(val, {
+    stiffness: 330,
+    damping: 22
+  });
+}
+
+// child matches will...
+const bounceTransition = {
+  // start in a transparent, upscaled state
+  atEnter: {
+    opacity: 0,
+    scale: 1.2
+  },
+  // leave in a transparent, downscaled state
+  atLeave: {
+    opacity: bounce(0),
+    scale: bounce(0.8)
+  },
+  // and rest at an opaque, normally-scaled state
+  atActive: {
+    opacity: bounce(1),
+    scale: bounce(1)
+  }
+};
+
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Header />
+    <div className="App">
+      <Header />
+      <AnimatedSwitch
+        atEnter={bounceTransition.atEnter}
+        atLeave={bounceTransition.atLeave}
+        atActive={bounceTransition.atActive}
+        mapStyles={mapStyles}
+        className="route-wrapper"
+      >
         <Route path="/blog" exact component={Blog} />
         <Route path="/photo" exact component={Photo} />
-      </div>
-    </Router>
+      </AnimatedSwitch>
+    </div>
   );
 }
 
